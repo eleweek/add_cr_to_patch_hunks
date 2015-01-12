f7ffe8e38b1ab8f2d5eb20bac6e5bd132600d9a8
Alexander Putilin <avp-13@yandex.ru>
2015-01-12 23:50:04 +0300

    Add --extra_cr option

--- add_cr_to_patch_hunks.py
+++ add_cr_to_patch_hunks.py
@@ -3,10 +3,13 @@ import argparse
 import re
 
 parser = argparse.ArgumentParser(description='Adds CR to patch')
+parser.add_argument('--extra_cr', dest='extra_cr', action='store_true', default=False, help="Terminate hunk lines with CRCRLF. Sounds weird, but this is a workaround for gist.github.com, because it replaces CRLF -> LF(so CRCRLF becomes CRLF)")
 parser.add_argument('input_diff_filename', help='diff_to_modify')
 parser.add_argument('output_diff_filename', help='diff_to_modify')
 
 args = parser.parse_args()
+
+hunk_line_terminator = '\r\r\n' if args.extra_cr else '\r\n'
 
 with open(args.input_diff_filename, 'r') as f:
     lines = f.readlines()
@@ -16,7 +19,7 @@ in_hunk = False
 for line in lines:
     if not re.match(r'^[ +-]', line):
         in_hunk = False
-    modified_lines.append(line.replace('\n', '\r\n') if in_hunk else line)
+    modified_lines.append(line.replace('\n', hunk_line_terminator) if in_hunk else line)
     if line.startswith("@@"):
         in_hunk = True
 
